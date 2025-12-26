8fb7c315759b2fdadcc2b662f24361abf59d2be8
Pierre0104 <pierredurieux@live.fr>
2025-12-26 17:57:19 +0100

    feat(back): integrate user endpoints in API server

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { PostgresCardRepository } from './infrastructure/repositories/PostgresCardRepository.js';
+import { PostgresUserRepository } from './infrastructure/repositories/PostgresUserRepository.js';
 import initDatabase from './infrastructure/database/initDatabase.js';
 import { CreateCard } from './application/usecases/CreateCard.js';
 import { GetCard } from './application/usecases/GetCard.js';
@@ -8,8 +9,15 @@ import { ReviewCard } from './application/usecases/ReviewCard.js';
 import { DeleteCard } from './application/usecases/DeleteCard.js';
 import { GetQuizzCards } from './application/usecases/GetQuizzCards.js';
 import { AnswerCard } from './application/usecases/AnswerCard.js';
+import { CreateUser } from './application/usecases/CreateUser.js';
+import { GetUser } from './application/usecases/GetUser.js';
+import { GetAllUsers } from './application/usecases/GetAllUsers.js';
+import { UpdateUser } from './application/usecases/UpdateUser.js';
+import { DeleteUser } from './application/usecases/DeleteUser.js';
 import { CardController } from './interfaces/http/controllers/CardController.js';
+import { UserController } from './interfaces/http/controllers/UserController.js';
 import { createCardRoutes } from './interfaces/http/routes/cardRoutes.js';
+import { createUserRoutes } from './interfaces/http/routes/userRoutes.js';
 
 export * from './domain/index.js';
 
@@ -18,24 +26,34 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
-const repository = new PostgresCardRepository();
-const createCard = new CreateCard(repository);
-const getCard = new GetCard(repository);
-const getAllCards = new GetAllCards(repository);
-const reviewCard = new ReviewCard(repository);
-const deleteCard = new DeleteCard(repository);
-const getQuizzCards = new GetQuizzCards(repository);
-const answerCard = new AnswerCard(repository);
+const cardRepository = new PostgresCardRepository();
+const createCard = new CreateCard(cardRepository);
+const getCard = new GetCard(cardRepository);
+const getAllCards = new GetAllCards(cardRepository);
+const reviewCard = new ReviewCard(cardRepository);
+const deleteCard = new DeleteCard(cardRepository);
+const getQuizzCards = new GetQuizzCards(cardRepository);
+const answerCard = new AnswerCard(cardRepository);
 const cardController = new CardController(createCard, getCard, getAllCards, reviewCard, deleteCard, getQuizzCards, answerCard);
 const cardRoutes = createCardRoutes(cardController);
 
+const userRepository = new PostgresUserRepository();
+const createUser = new CreateUser(userRepository);
+const getUser = new GetUser(userRepository);
+const getAllUsers = new GetAllUsers(userRepository);
+const updateUser = new UpdateUser(userRepository);
+const deleteUser = new DeleteUser(userRepository);
+const userController = new UserController(createUser, getUser, getAllUsers, updateUser, deleteUser);
+const userRoutes = createUserRoutes(userController);
+
 app.use('/api', cardRoutes);
+app.use('/api', userRoutes);
 
 app.get('/health', (req: Request, res: Response) => {
   res.json({ status: 'ok' });
 });
 
-// Initialize database and start server
+
 initDatabase()
   .then(() => {
     app.listen(PORT, () => {
